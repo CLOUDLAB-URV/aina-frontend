ee7bee96b4397fc2795c8bceb86f334529b0fcd1
Iulian Sebastian Oprea <iulianoprea14@gmail.com>
2025-10-14 11:19:59 +0200

    Main Container

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -57,12 +57,12 @@ function isOutsideClicked(event: any) {
     <div class="layout-wrapper" :class="containerClass">
         <app-topbar></app-topbar>
         <app-sidebar></app-sidebar>
-        <!-- <div class="layout-main-container">
+        <div class="layout-main-container">
             <div class="layout-main">
                 <router-view></router-view>
             </div>
         </div>
-        <div class="layout-mask animate-fadein"></div> -->
+        <div class="layout-mask animate-fadein"></div>
     </div>
     <!-- <Toast style="top: 71px;" /> -->
 </template>
