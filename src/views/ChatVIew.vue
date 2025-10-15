074b7bfbfc450a58eacf32da8b8c61b10ff59c01
Iulian Sebastian Oprea <iulianoprea14@gmail.com>
2025-10-15 13:12:11 +0200

    ChatVIew

--- src/views/ChatVIew.vue
+++ src/views/ChatVIew.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex">
         <div class="w-1/4 flex flex-col">
-            <SelectorDropdown :label="`Agent`" :options="dataProva" />
+            <AgentSelect class="col-start-1" />
             <AgentSelect class="col-start-1" />
             <AgentSelect class="col-start-1" />
         </div>
@@ -12,9 +12,4 @@
 <script setup lang="ts">
 import AgentSelect from '@/components/AgentSelect.vue';
 import ChatComponent from '@/components/ChatComponent.vue';
-import SelectorDropdown from '@/components/SelectorDropdown.vue';
-
-const dataProva = [
-    "Agent 1", "Agent2", "Agent3", "Agent4"
-]
 </script>
