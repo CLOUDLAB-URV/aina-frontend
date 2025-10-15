c06ad63bf39601184bf19ab7794a7a89feb34514
Iulian Sebastian Oprea <iulianoprea14@gmail.com>
2025-10-15 13:21:11 +0200

    Futures pourpuses when api defined

---
+++ src/components/SelectorDropdown.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex flex-col justify-between p-2">
+        <h2 class="mb-4 text-lg">{{ label }}</h2>
+        <select name="agent" id="agent">
+            <option v-for="option in options" :value="option">{{ option }}</option>
+        </select>
+        <div class="flex justify-between mt-4">
+            <Button icon="pi pi-trash" severity="danger"></Button>
+            <Button icon="pi pi-pencil" severity="info"></Button>
+            <Button icon="pi pi-file-plus" severity="success"></Button>
+        </div>
+    </div>
+</template>
+<script lang="ts" setup>
+defineProps<{
+    label: string;
+    options: string[];
+}>();
+</script>
