db2fd11dbc308dd839bc82734338fbaa43880d5f
Iulian Sebastian Oprea <iulianoprea14@gmail.com>
2025-10-16 08:40:25 +0200

    APIS and Models OpenApi

---
+++ src/apis/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AgentsApi';
+export * from './AuthenticationApi';
+export * from './ConversationsApi';
+export * from './EmbeddingsApi';
+export * from './LlmsApi';
+export * from './RerankingsApi';
---
+++ src/models/index.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Agent';
+export * from './AgentInfo';
+export * from './ConversationCreate';
+export * from './ConversationInfo';
+export * from './ConversationUpdate';
+export * from './EmbeddingCreate';
+export * from './EmbeddingInfo';
+export * from './GenericException';
+export * from './HTTPValidationError';
+export * from './LlmCreate';
+export * from './LlmInfo';
+export * from './RerankingCreate';
+export * from './RerankingInfo';
+export * from './Role';
+export * from './Token';
+export * from './UserInfo';
+export * from './ValidationError';
+export * from './ValidationErrorLocInner';
