0b530337ee57fada6a022ed6c7427b12b6322083
Iulian Sebastian Oprea <iulianoprea14@gmail.com>
2025-11-04 11:45:03 +0100

    Automatized Create Agent

--- src/components/ModalCreateAgent.vue
+++ src/components/ModalCreateAgent.vue
@@ -1,13 +1,15 @@
-
 <template>
     <div class="card flex justify-center">
-        <button v-if="create == 'create'" @click="visible = true" class="bg-green-400 hover:bg-green-600 p-2 flex flex-col justify-center rounded-lg" id="btn-create">
+        <button v-if="create == 'create'" @click="visible = true"
+            class="bg-green-400 hover:bg-green-600 p-2 flex flex-col justify-center rounded-lg" id="btn-create">
             <i class="pi pi-file-plus"></i>
         </button>
-        <button v-if="create == 'trash'" @click="visible = true" class="bg-red-400 hover:bg-red-400 p-2 flex flex-col justify-center rounded-lg" id="btn-delete">
+        <button v-if="create == 'trash'" @click="visible = true"
+            class="bg-red-400 hover:bg-red-400 p-2 flex flex-col justify-center rounded-lg" id="btn-delete">
             <i class="pi pi-trash"></i>
         </button>
-        <button v-if="create == 'edit'" @click="visible = true" class="bg-blue-400 hover:bg-blue-400 p-2 flex flex-col justify-center rounded-lg" id="btn-edit">
+        <button v-if="create == 'edit'" @click="visible = true"
+            class="bg-blue-400 hover:bg-blue-400 p-2 flex flex-col justify-center rounded-lg" id="btn-edit">
             <i class="pi pi-pencil"></i>
         </button>
         <Dialog v-model:visible="visible" modal header="Create an Agent" :style="{ width: '40rem' }">
@@ -17,116 +19,161 @@
             </div>
             <div class="flex items-center gap-4 mb-2">
                 <label for="desc" class="font-semibold w-24">Description</label>
-                <textarea v-model="agent.description" id="desc" class="flex-auto border border-gray-200 p-2" autocomplete="off"  />
+                <textarea v-model="agent.description" id="desc" class="flex-auto border border-gray-200 p-2"
+                    autocomplete="off" />
             </div>
+
             <div class="flex items-center gap-4 mb-2">
                 <label for="modelName" class="font-semibold w-24">ModelName</label>
-                <InputText v-model="agent.modelName" id="modelName" class="flex-auto" autocomplete="off" />
-            </div>            
-            <div class="flex items-center gap-4 mb-2">
+                <select id="modelName" v-model="agent.modelName" class="w-full">
+                    <option value="">Please Select an Option</option>
+                    <option :value="model" v-for="model in modelsNames">{{ model }}</option>
+                </select>
+            </div>
+
+            <div class="flex items-center gap-4 mb-2" v-if="create == 'edit'">
                 <label for="indexId" class="font-semibold w-24">IndexId</label>
                 <input type="number" v-model="agent.indexId" id="indexId" class="flex-auto" autocomplete="off" />
-            </div>            
+            </div>
             <template #footer>
                 <Button label="Cancel" text severity="secondary" @click="visible = false" autofocus />
-                <Button v-if="create == 'create'" label="Create" variant="outlined" severity="secondary" @click="createAgent" autofocus />
-                <Button v-if="create == 'trash'" label="Delete" variant="outlined" severity="secondary" @click="deleteAgent" autofocus />
-                <Button v-if="create == 'edit'" label="Edit" variant="outlined" severity="secondary" @click="updateAgent" autofocus />
+                <Button v-if="create == 'create'" label="Create" variant="outlined" severity="secondary"
+                    @click="createAgent" autofocus />
+                <Button v-if="create == 'trash'" label="Delete" variant="outlined" severity="secondary"
+                    @click="deleteAgent" autofocus />
+                <Button v-if="create == 'edit'" label="Edit" variant="outlined" severity="secondary"
+                    @click="updateAgent" autofocus />
             </template>
         </Dialog>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch ,onMounted} from 'vue';
-// import type { AgentCreate } from '@/models/AgentCreate.ts';
+import { ref, watch, onMounted } from 'vue';
 import type { AgentUpdate } from '@/models/AgentUpdate.ts';
 import { AgApi } from "@/apis/api";
-import type { AddAgentApiV1AgentsPostRequest,UpdateAgentApiV1AgentsAgentIdPatchRequest,DeleteAgentApiV1AgentsAgentIdDeleteRequest } from "@/apis";
+import type { AddAgentApiV1AgentsPostRequest, UpdateAgentApiV1AgentsAgentIdPatchRequest, DeleteAgentApiV1AgentsAgentIdDeleteRequest } from "@/apis";
 import type { AgentCreate } from '@/models';
 import { useAgentStore } from '@/stores/agent';
+import { IndApi } from '@/apis/api';
+import type { CreateIndexApiV1IndexPostRequest } from '@/apis/IndexApi';
+import { useToast } from 'primevue/usetoast';
+import { LlmApi } from '@/apis/api';
 
+const toast = useToast();
 const store = useAgentStore();
 const props = defineProps({
-  create: { type: String, required: true },
-  data: Object,
+    create: { type: String, required: true },
+    data: Object,
 });
+let modelsNames = ref();
 
 const visible = ref(false);
 
 let agent = ref({
-  name: "",
-  description: "",
-  indexId: 0,
-  modelName: "",
-  id: ""
+    name: "",
+    description: "",
+    indexId: 0,
+    modelName: "",
+    id: ""
 })
 
 onMounted(async () => {
-    console.log(props.data)
-  change_model();
-});
+    try {
+        const response = await LlmApi.listLlmsApiV1LlmsGet();
+        if (response) {
+            modelsNames.value = Object.entries(response)?.map((llm: any) => {
+                return llm[0]
+            });
+        }
+    } catch (error) {
+        console.error("Failed to retrieve LLMs:", error);
+    }
+})
 
 watch(() => props.data, async () => {
     console.log(props.data)
-  change_model();
-});
+    change_model();
+}, { immediate: true });
 
 
 function change_model() {
-  agent.value.name = props.data?.name ?? "";
-  agent.value.description = props.data?.description ?? "";
-  agent.value.indexId = props.data?.indexId ?? 0;
-  agent.value.modelName = props.data?.modelName ?? "";
-  agent.value.id = props.data?.id ?? "";
+    agent.value.name = props.data?.name ?? "";
+    agent.value.description = props.data?.description ?? "";
+    agent.value.indexId = props.data?.indexId ?? 0;
+    agent.value.modelName = props.data?.modelName ?? "";
+    agent.value.id = props.data?.id ?? "";
 }
 
 async function createAgent() {
     console.log(agent.value);
-    let agentCreate : AgentCreate = {
-        name : agent.value.name,
-        description :  agent.value.description,
-        indexId : agent.value.indexId,
-        modelName : agent.value.modelName,
+    let agentCreate: AgentCreate = {
+        name: agent.value.name,
+        description: agent.value.description,
+        indexId: agent.value.indexId,
+        modelName: agent.value.modelName,
     }
-    let info: AddAgentApiV1AgentsPostRequest = { agentCreate: agentCreate};
+    let info: AddAgentApiV1AgentsPostRequest = { agentCreate: agentCreate };
     await AgApi.addAgentApiV1AgentsPost(info).then(
-        async (res) => {
-            let updateData : AgentUpdate = {
-                indexId : agent.value.indexId,
-                modelName : agent.value.modelName
-            }
-            let update : UpdateAgentApiV1AgentsAgentIdPatchRequest = {
-                agentId : res.id,
-                agentUpdate : updateData
-            }
-            await AgApi.updateAgentApiV1AgentsAgentIdPatch(update).then((res)=>{
-                visible.value = false;
-                store.data.push(res)
+        (agRes) => {
+
+            let indexInfo: CreateIndexApiV1IndexPostRequest = {
+                name: `${agent.value.name}_index`,
+                indexType: 'FileIndex',
+                requestBody: {
+                    "embedding": "default",
+                    "supported_file_types": ".pdf, .md",
+                    "max_file_size": 1000,
+                    "max_number_of_files": 0,
+                    "private": true,
+                    "chunk_size": 0,
+                    "chunk_overlap": 0
+                }
+            };
+
+            IndApi.createIndexApiV1IndexPost(indexInfo).then(async (res) => {
+                let updateData: AgentUpdate = {
+                    indexId: res.id,
+                    modelName: agent.value.modelName
+                }
+                let update: UpdateAgentApiV1AgentsAgentIdPatchRequest = {
+                    agentId: agRes.id,
+                    agentUpdate: updateData
+                }
+                await AgApi.updateAgentApiV1AgentsAgentIdPatch(update).then((res) => {
+                    visible.value = false;
+                    store.data.push(res)
+                    toast.add({
+                        severity: 'success',
+                        summary: 'Agent has been created successfully',
+                        detail: `Agent has been created ${agent.value.name}`,
+                        life: 4000
+                    })
+                })
             })
         }
     );
 }
-async function updateAgent(){
-    let updateData : AgentUpdate = {
-        indexId : agent.value.indexId,
-        modelName : agent.value.modelName,
-        name:agent.value.name,
-        description:agent.value.description
+async function updateAgent() {
+    let updateData: AgentUpdate = {
+        indexId: agent.value.indexId,
+        modelName: agent.value.modelName,
+        name: agent.value.name,
+        description: agent.value.description
     }
-    let update : UpdateAgentApiV1AgentsAgentIdPatchRequest = {
-        agentId : agent.value.id,
-        agentUpdate : updateData
+    let update: UpdateAgentApiV1AgentsAgentIdPatchRequest = {
+        agentId: agent.value.id,
+        agentUpdate: updateData
     }
-    await AgApi.updateAgentApiV1AgentsAgentIdPatch(update).then(()=>{
+    await AgApi.updateAgentApiV1AgentsAgentIdPatch(update).then(() => {
         visible.value = false;
     })
 }
-async function deleteAgent(){
-    let deleteData : DeleteAgentApiV1AgentsAgentIdDeleteRequest = {
-        agentId : agent.value.id,
+async function deleteAgent() {
+    let deleteData: DeleteAgentApiV1AgentsAgentIdDeleteRequest = {
+        agentId: agent.value.id,
     }
-    await AgApi.deleteAgentApiV1AgentsAgentIdDelete(deleteData).then(()=>{
+    await AgApi.deleteAgentApiV1AgentsAgentIdDelete(deleteData).then(() => {
         visible.value = false;
         store.removeAgent(agent.value.id);
     })
@@ -134,17 +181,21 @@ async function deleteAgent(){
 
 </script>
 <style scoped>
-#btn-create,#btn-edit,#btn-delete{
+#btn-create,
+#btn-edit,
+#btn-delete {
     transition: box-shadow 0.2s ease-in-out;
 }
-#btn-create:hover{
+
+#btn-create:hover {
     box-shadow: 0px 0px 16px var(--color-green-400);
 }
-#btn-edit:hover{
+
+#btn-edit:hover {
     box-shadow: 0px 0px 16px var(--color-blue-400);
 }
-#btn-delete:hover{
+
+#btn-delete:hover {
     box-shadow: 0px 0px 16px var(--color-red-400);
 }
-
 </style>
--- src/views/ChatVIew.vue
+++ src/views/ChatVIew.vue
@@ -1,15 +1,9 @@
 <template>
     <div class="flex">
-        <div class="w-1/4 flex flex-col">
-            <AgentSelect class="col-start-1" />
-            <AgentSelect class="col-start-1" />
-            <AgentSelect class="col-start-1" />
-        </div>
         <ChatComponent class="h-full grow" />
     </div>
 </template>
 
 <script setup lang="ts">
-import AgentSelect from '@/components/AgentSelects.vue';
 import ChatComponent from '@/components/ChatComponent.vue';
 </script>
